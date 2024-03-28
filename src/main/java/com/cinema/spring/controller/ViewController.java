5629e999530af0de22cb4c989758a3675fd4ada4
leppsalujyrgen <leppsalujyrgen@gmail.com>
2024-03-28 19:16:11 +0200

    Modified Application controller to use services classes.

--- src/main/java/com/cinema/spring/controller/ViewController.java
+++ src/main/java/com/cinema/spring/controller/ViewController.java
@@ -6,27 +6,34 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.cinema.spring.repository.GenreRepository;
-import com.cinema.spring.repository.MovieRepository;
+import com.cinema.spring.service.GenreService;
+import com.cinema.spring.service.MovieService;
 
 @Controller
 public class ViewController {
 	
 	@Autowired
-	private MovieRepository movieRepository;
+	private final MovieService movieService;
+	
 	@Autowired
-    private GenreRepository genreRepository;
+	private final GenreService genreService;
+  
+	@Autowired
+	public ViewController(MovieService movieService, GenreService genreService) {
+      this.movieService = movieService;
+      this.genreService = genreService;
+	}
 	
 	@GetMapping("/")
 	public String getHomePage(Model model) {
-		model.addAttribute("movies", movieRepository.findAll());
+		model.addAttribute("movies", movieService.getAllMovies());
 		return "index";
 	}
 	
 	@GetMapping("/movies")
 	public String getMoviesPage(Model model) {
-		model.addAttribute("movies", movieRepository.findAll());
-		model.addAttribute("genres", genreRepository.findAll());
+		model.addAttribute("movies", movieService.getAllMovies());
+		model.addAttribute("genres", genreService.getAllGenres());
 		return "movies";
 	}
 
